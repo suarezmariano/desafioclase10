a5e2ab33ccbc252b3407bb3220dafdee99ed77cc
suarezmariano <marianosuarez@hotmail.com>
2022-03-21 00:06:02 -0300

    ejs terminado

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ const router = express.Router();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const productos = [];
+
 //SERVER
 const port = 8080;
 const server = app.listen(port, () => {
-  console.log('servidor levantado en el puerto: ' + port);
+  console.log('Servidor corriendo en el puerto: ' + port);
 });
 server.on('error', (error) => console.log(`hubo un error ${error}`));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('formulario', { personas });
+  res.render('formulario', { productos });
 });
 
 app.post('/productos', (req, res) => {
-  console.log(req.body);
-  personas.push(req.body);
+  productos.push(req.body);
   res.render('formulario', { productos });
 });
