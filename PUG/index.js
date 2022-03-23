0dbeb9a4c1ce456e232a81ad285b623758df8c50
suarezmariano <marianosuarez@hotmail.com>
2022-03-22 22:49:38 -0300

    creado de archivos pug

---
+++ PUG/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const router = express.Router();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const productos = [];
+
+//SERVER
+const port = 8080;
+const server = app.listen(port, () => {
+  console.log('Servidor corriendo en el puerto: ' + port);
+});
+server.on('error', (error) => console.log(`hubo un error ${error}`));
